0f36d43f0a73a00a23de83906fbe3ae9a1f05ccb
scott pittman <sp.digital97@gmail.com>
2021-03-25 06:56:08 -0400

    saving all node packages for rtv to github with server requires

---
+++ rtv/server.js
@@ -0,0 +1,6 @@
+const express = require("express")
+const app = express()
+require('dotenv').config()
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const expressjwt = require('expressjwt')
